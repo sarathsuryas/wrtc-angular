a7d6404eebb821da9583036647522da79aaa22af
sarathsuryas <sarathsuryasss48@gmail.com>
2025-03-16 18:57:43 +0530

    nest domain added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { BroadcasterComponent } from './broadcaster/broadcaster.component';
 import { ViewerComponent } from './viewer/viewer.component';
 
 export const routes: Routes = [
+    {path:'',pathMatch:'full',redirectTo:'broadcast'},
     {path:'broadcast',component:BroadcasterComponent},
     {path:'view',component:ViewerComponent}
 ];
